574d6945a5ea01bdbd72b47db5f694f370cbd0a2
lan-tianxiang <42911023+lan-tianxiang@users.noreply.github.com>
2021-01-31 18:39:46 +0800

    Update jd_nian_out.js

--- jd_nian_out.js
+++ jd_nian_out.js
@@ -97,8 +97,7 @@ async function jdNian() {
     if (1 <= hour && hour < 12) {
       // 北京时间9点-20点
       $.hasGroup = false
-//      await pkTaskDetail()
-      console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
+      await pkTaskDetail()
       if ($.hasGroup) await pkInfo()
     }
     if (12 <= hour && hour < 14) {
