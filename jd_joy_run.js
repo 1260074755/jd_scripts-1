262adfa9e447036ada5cf076cc232e800de7b49a
lan-tianxiang <42911023+lan-tianxiang@users.noreply.github.com>
2021-02-18 11:20:25 +0800

    Update jd_joy_run.js

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -45,7 +45,7 @@ let run_pins = ["jd_DyPZZzNZQFxZ,q450878-912928,jd_kuIoyQouhCLm,jd_qvAijclDYexz,
 let temp = run_pins[0].split(',')
 let fixPins = temp.splice(temp.indexOf('whoami'), 1);
 fixPins.push(...temp.splice(temp.indexOf('WHOAMI'), 1));
-const randomPins = getRandomArrayElements(temp, 0);
+const randomPins = getRandomArrayElements(temp, 1);
 temp = [...fixPins, ...randomPins];
 run_pins = [temp.join(',')];
 // $.LKYLToken = '76fe7794c475c18711e3b47185f114b5' || $.getdata('jdJoyRunToken');
