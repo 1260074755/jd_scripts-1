5715f0ae0c1baad6d4cc57ff91e0fd3b6d78580d
lan-tianxiang <42911023+lan-tianxiang@users.noreply.github.com>
2021-02-18 14:46:50 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -2,4 +2,4 @@
 #Joy_run
 18 11,14 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
 #test
-30 8-20/4 * * * bash /home/myid/jd/jd.sh jd_nian >> /scripts/logs/jd_nian.log 2>&1
+30 8-20/4 * * * node /home/myid/jd/jd.sh jd_nian >> /scripts/logs/jd_nian.log 2>&1
