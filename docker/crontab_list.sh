b9ae8ec066c40cf1e30aea2315268abb1ba2a4aa
lan-tianxiang <42911023+lan-tianxiang@users.noreply.github.com>
2021-02-19 22:24:55 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,2 +1,5 @@
 #暂无
 0 0,8,12,16 * * * node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.js 2>&1
+
+# 宠汪汪邀请助力
+10 9-20/2 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
